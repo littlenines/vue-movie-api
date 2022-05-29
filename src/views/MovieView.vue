7a24b3843e0732e6d08c6f88999208482803c546
Nemanja <nemanja.galbinovic@badin.rs>
2022-05-29 22:41:59 +0200

    can't continue from here

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,17 +1,47 @@
 <template>
-    <nav>
+  <nav>
     <router-link to="/Movies">Movies</router-link>
   </nav>
   <div class="about">
-    <Genre />
+    <!-- <Genre /> -->
+    <div data-idk>
+      
+    </div>
   </div>
 </template>
 
 <script>
-import Genre from '@/components/MovieGenre.vue'
+import Genre from "@/components/MovieGenre.vue";
+import genres from "../../genres.json";
+import { getMoviesApi } from "@/utils/movieApi";
 export default {
-    components: {
-    Genre
-  }
-}
+  components: {
+    Genre,
+  },
+  data() {
+    return {
+      getMovieGenres: genres.genres,
+      idk: [],
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+    async getGenres() {
+      const aa = await Promise.all( // what now ????????????????????
+        this.getMovieGenres.map(async (genres) => {
+          const bb = await getMoviesApi(genres.id);
+          return bb.results.slice(0, 5);
+        })
+      );
+      console.log(aa);
+      this.idk = aa;
+    },
+  },
+  created() {
+    this.getGenres();
+    // this.getMovies();
+  },
+};
 </script>
